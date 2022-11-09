6fa7574cee4932470f7a6a7c7bac59b188b679fc
Intern-Yago <yagovictorbotafogo@gmail.com>
2022-11-09 15:16:42 -0300

    nodeChat

---
+++ README.md
@@ -0,0 +1,30 @@
+# NodeChat [![Build Status](https://img.shields.io/badge/status-build-orange)](https://github.com/Intern-Yago)
+<h6 align="right">
+  Version 1.0
+</h6>
+
+![alt text](https://media.giphy.com/media/WAnUdXKHObQUU/giphy.gif)
+
+### NodeChat é um chat web feito com:
+- Socket.io
+- Express
+- JQuery
+
+
+---
+### Atualizações a serem feitas
+[  ] Apagar bate papo após certo período ou limite de conversa
+
+[  ] Adaptar electron
+
+[  ] Integrar banco de dados
+
+[  ] Colocar autenticação
+
+[  ] Bloquear rotas
+
+[  ] Opção de o usuário criar salas
+
+[  ] Sala privada
+
+No hidden costs or surprises Cancel or change your plan at any time.
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+var messages = []
+let conectados = 0
+
+app.set('views', path.join(__dirname, 'public'))
+app.engine('html', require('ejs').renderFile)
+app.set("view engine", 'html')
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('/', (req,res)=> res.render('index.html'))
+
+io.on('connection', socket=>{
+    socket.emit('previousMessages', messages)
+    conectados = conectados + 1
+    socket.on('sendMessage', data =>{
+        messages.push(data)
+        socket.broadcast.emit('receiveMessage', data)
+    })
+
+    socket.on('disconnect', function(){
+        conectados = conectados - 1
+        if(conectados === 0)
+            messages = []
+        
+      })
+})
+
+
+server.listen(process.env.PORT || 3000, ()=>{
+    console.log("Servidor rodando!\nVisit: http://localhost:3000\n")
+})
